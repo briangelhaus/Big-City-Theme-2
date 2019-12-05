1ae80de38a1c05383cf12eb11bdba9c67543a0f0
Brian Gelhaus <briangelhaus@gmail.com>
2019-12-05 12:38:05 -0500

    sidebar accepts all categories

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,13 @@
 <div class="sidebar">
 	<h3>Categories</h3>
+	<?php  
+		$product_cats = get_terms(array(
+		    'taxonomy' => 'category',
+		    'hide_empty' => false,
+		));
+	?>
 	<ul>
-		<?php foreach(get_categories() as $c): ?>
+		<?php foreach($product_cats as $c): ?>
 		<li><a href="<?php echo get_category_link($c->term_id); ?>"><?php echo $c->name; ?></a></li>
 		<?php endforeach; ?>
 	</ul>
