f7a4d9006015deff3defd58a9bc59718c5f89095
Brian Gelhaus <briangelhaus@gmail.com>
2020-01-07 14:21:17 -0500

    style guide

--- page-style-guide.php
+++ page-style-guide.php
@@ -7,6 +7,11 @@
 		<div class="row">
 			<div class="col-lg-12">
 				
+				<!-- 
+					create a wordpress page called "style guide" to view this. 
+					you'll want to add on to this page 
+				-->
+				
 				<!-- basic use -->
 				<h1>Font Style 1 as an h1</h1>
 				<!-- styling as another heading -->
@@ -16,6 +21,11 @@
 					<h2>Font Style 1 - used for wysiwyg</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
+				
+				<p>
+					<a class="btn" href="">Button 1</a>
+					<a class="btn btn-2" href="">Button 2</a>
+				</p>
 				
 				<h2>Font Style 2 as an h2</h2>
 				<h5 class="fs2">Font Style 2 class on an h5</h5>
